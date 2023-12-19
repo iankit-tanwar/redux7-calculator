7c37b6c6cea68c7e07bc4aaa7f8ac1c76ef01fa3
Ankit Tanwar <tankitofficial01@gmail.com>
2023-12-19 21:20:36 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { number } from '.';
 
 function App() {
+const state = useSelector((storeObj)=>{
+  return storeObj
+})
+
+const dispatch = useDispatch()
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+     {/* Calculator Body */}
+<div className="calc-body ">
+  {/* Calculator Display Screen */}
+  <div className="calc-screen">
+   
+    <div id="calc-typed"> {state.result  } </div>
+  </div>
+  {/* Calculator Buttons */}
+  <div className="calc-button-row">
+    <button className="ac" onClick={()=>{dispatch({type:'CLEAR'})}}>AC</button>
+    <button className="opt" >+/−</button>
+    <button className="opt"  onClick={()=>{dispatch({type:'PERCENTAGE'})}}>%</button>
+    <button className="opt"  onClick={()=>{dispatch({type:'DIVISION'})}}>÷</button>
+    <button value={7}onClick={(e)=>{dispatch(number(e.target.value))}}>7</button>
+    <button value={8}onClick={(e)=>{dispatch(number(e.target.value))}}>8</button>
+    <button value={9}onClick={(e)=>{dispatch(number(e.target.value))}}>9</button>
+    <button className="opt"  onClick={()=>{dispatch({type:'MULTIPLICATION'})}}>*</button>
+    <button value={4}onClick={(e)=>{dispatch(number(e.target.value))}}>4</button>
+    <button value={5}onClick={(e)=>{dispatch(number(e.target.value))}}>5</button>
+    <button value={6}onClick={(e)=>{dispatch(number(e.target.value))}}>6</button>
+    <button className="opt"  onClick={()=>{dispatch({type:'SUBTRACTION'})}}>−</button>
+    <button value={1} onClick={(e)=>{dispatch(number(e.target.value))}}>1</button>
+    <button value={2}onClick={(e)=>{dispatch(number(e.target.value))}}>2</button>
+    <button value={3}onClick={(e)=>{dispatch(number(e.target.value))}}>3</button>
+    <button className="opt"  onClick={()=>{dispatch({type:'ADDITION'})}}>+</button>
+    <button value={0}onClick={(e)=>{dispatch(number(e.target.value))}}>0</button>
+    <button>.</button>
+    <button>⌫</button>
+    <button className="opt" onClick={()=>{dispatch({type:'RESULT'})}}>=</button>
+  </div>
+</div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,115 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
+
+let rootReducer = (preState, action) => {
+  let newState = preState;
+
+
+  switch (action.type) {
+
+    case 'ADDITION':
+      //alert("addition")
+      newState.operator = '+'
+      break;
+    case 'SUBTRACTION':
+      //alert("SUBTRACTION")
+      newState.operator = '-'
+      break;
+    case 'MULTIPLICATION':
+      // alert('MULTIPLICATION')
+      newState.operator = '*'
+      break;
+    case 'DIVISION':
+      //alert('DIVISION')
+      newState.operator = '/'
+      break;
+    case 'RESULT':
+
+
+      switch (newState.operator) {
+        case '+':
+          newState.result = newState.leftValue + newState.rightValue
+          break;
+        case '-':
+          newState.result = newState.leftValue - newState.rightValue
+          break;
+        case '*':
+          newState.result = newState.leftValue * newState.rightValue
+          break;
+        case '/':
+          newState.result = newState.leftValue / newState.rightValue
+          break;
+      }
+
+      break;
+    case 'CLEAR':
+      // alert('CLEAR')
+      newState.result = 0
+      newState.leftValue = ''
+      newState.rightValue = ''
+      newState.operator = ''
+      
+      break;
+
+    case 'NUMBER':
+      // alert(action.number)
+
+      console.log(newState)
+
+      if (newState.operator === '') {
+        if (newState.leftValue === '') {
+          newState.leftValue = action.number
+        } else {
+          //concatination
+
+          newState.leftValue = parseInt(newState.leftValue + action.number)
+          console.log(typeof newState.leftValue)
+
+        }
+      } else {
+        if (newState.operator === '') {
+          newState.rightValue = action.number
+        } else {
+          newState.rightValue = parseInt(newState.rightValue + action.number)
+        }
+
+      }
+
+      break;
+  }
+
+console.log(newState.result)
+  return newState;
+}
+
+export const number = (n) => {
+
+  return {
+    type: 'NUMBER',
+    number: n
+  }
+}
+
+const storeObj = createStore(rootReducer, {
+  result: 0,
+  leftValue: '',
+  operator: '',
+  rightValue: ''
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={storeObj}>
+      <App />
+
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
